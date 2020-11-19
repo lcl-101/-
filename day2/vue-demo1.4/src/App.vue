530528ee936ed1a51e59c4ee975670440b8c01b1
lichenglong <554675188@qq.com>
2020-11-19 11:32:00 +0800

    ++

---
+++ day2/vue-demo1.4/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p>
+      <button @click="handleNameChange">change this.name</button>
+      <button @click="handleInfoChange">change this.info</button>
+      <button @click="handleListChange">change this.list</button>
+    </p>
+    <PropsAndData :name="name" :info="info" :list="list" />
+  </div>
+</template>
+
+<script>
+
+import PropsAndData from "./components/PropsAndData";
+export default {
+  name: 'App',
+  components: {
+    PropsAndData
+  },
+  data() {
+    return {
+      name: this.name,
+      info: {
+        number: undefined
+      },
+      // info: {
+      //   number: undefined
+      // },
+      list: []
+    }
+  },
+  methods: {
+    handleNameChange() {
+      this.name = "vue" + Date.now();
+      console.log("this.name 发生了变化，但是并没有触发子组件更新", this.name);
+    },
+    handleInfoChange() {
+      this.info.number = 1;
+      // this.$set(this.info, 'number', 1)
+      console.log("this.info 发生了变化，但是并没有触发子组件更新", this.info);
+    },
+    handleListChange() {
+      this.list.push(1, 2, 3);
+      console.log("this.list 并没有发生变化，但是触发了子组件更新", this.list);
+    }
+  }
+}
+</script>

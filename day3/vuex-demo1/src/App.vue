d347f66a90c409ac21403b429e32ef121100299b
lichenglong <554675188@qq.com>
2020-12-16 18:51:14 +0800

    ++

---
+++ day3/vuex-demo1/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{count}}
+    <br>
+    {{$store.getters.doubleCount}}
+    <button @click="$store.commit('increment', 2)">count+2</button>
+    <button @click="$store.dispatch('increment')">count++</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  }
+}
+</script>
+
+<style>
+</style>

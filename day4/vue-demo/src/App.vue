819fff4cb60631c1a975595db7b0b8a569acc0d0
lichenglong04 <554675188@qq.com>
2021-05-21 14:18:04 +0800

    ++

---
+++ day4/vue-demo/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="message">
+    <input type="text" :value="message" @input="handelChange">
+    {{message}} {{message + message}}
+    <div :id="message"></div>
+    <todo-list>
+        <todo-item v-for="item in list" @delete="deletehandel" :key="item.name" :name="item.name" :del="item.del">
+            <template v-slot:pre-icon="{value}">
+                <span>前置图标{{value}}</span>
+            </template>
+            <template v-slot:suf-icon>
+                <!-- <span>后置图标</span> -->
+            </template>
+            <!-- vue2.5
+            <span slot="pre-icon">前置图标</span>
+            <span slot="suf-icon">后置图标</span> -->
+        </todo-item>
+    </todo-list>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoItem,
+    TodoList
+  },
+  data (){
+    return {
+      message: 'hello world',
+      list: [
+            {
+                name: '课程',
+                del: false
+            },
+            {
+                name: '课程2',
+                del: true
+            }
+        ]
+    }
+  },
+  methods: {
+    handelChange(e){
+      this.message = e.target.value
+    },
+    deletehandel(val){
+        console.log('deletehandel:',val)
+    }
+  }
+}
+</script>
---
+++ day4/vue-demo/src/components/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <li>
+        <slot name="pre-icon" :value="value"></slot>
+        <span class="red" v-if="!del">{{name}}</span>
+        <span v-else style="text-decoration: line-through;">{{name}}</span>
+        <slot name="suf-icon">😃</slot>
+        <button v-show="!del" @click="handelClick">删除</button>
+    </li>
+</template>
+<script>
+export default {
+    props: {
+        name: String,
+        del: Boolean
+    },
+    data: function(){
+        return {
+            value: Math.random()
+        }
+    },
+    methods: {
+        handelClick (){
+            this.$emit('delete', this.name);
+        }
+    }
+}
+</script>
+<style scoped>
+.red {
+  color: red;
+}
+</style>
